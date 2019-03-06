257c9436f9771598a07c632f4819599f21b7c8a9
prudhvir3ddy <prudhvireddy.m01@gmail.com>
2019-03-06 15:19:02 +0530

    new branch created

---
+++ git-demo/src/main/java/com/epam/gitdemo/newBranch.java
@@ -0,0 +1,13 @@
+package com.epam.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class newBranch 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello new git branch!" );
+    }
+}
